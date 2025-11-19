a3065dbcb981fc98896bc5c84e7439cb006ad5c9
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2025-11-19 14:20:06 -0500

    implement basic controller to trigger interactor and presenter to handle output data

--- src/main/java/interface_adapter/replacement/ReplacementController.java
+++ src/main/java/interface_adapter/replacement/ReplacementController.java
@@ -1,4 +1,20 @@
 package interface_adapter.replacement;
 
+import use_case.replacement.ReplacementInputBoundary;
+import use_case.replacement.ReplacementInputData;
+import entity.Team;
+
 public class ReplacementController {
+
+    private final ReplacementInputBoundary interactor;
+
+    public ReplacementController(ReplacementInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    // For future: team comes from the view
+    public void recommendReplacement(Team team) {
+        ReplacementInputData inputData = new ReplacementInputData(team);
+        interactor.execute(inputData);
+    }
 }
--- src/main/java/interface_adapter/replacement/ReplacementPresenter.java
+++ src/main/java/interface_adapter/replacement/ReplacementPresenter.java
@@ -1,4 +1,12 @@
 package interface_adapter.replacement;
 
-public class ReplacementPresenter {
+import use_case.replacement.ReplacementOutputBoundary;
+import use_case.replacement.ReplacementOutputData;
+
+public class ReplacementPresenter implements ReplacementOutputBoundary {
+
+    @Override
+    public void prepareSuccessView(ReplacementOutputData outputData) {
+        System.out.println("[Demo] Suggested Players:" + outputData.getSuggestedPlayers());
+    }
 }
