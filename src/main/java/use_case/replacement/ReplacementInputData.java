7281ece9318d0a237881e8f8a4d10ad65cb46362
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2025-11-19 13:19:33 -0500

    define input/output data structures for Replacement feature

--- src/main/java/use_case/replacement/ReplacementInputData.java
+++ src/main/java/use_case/replacement/ReplacementInputData.java
@@ -1,4 +1,15 @@
 package use_case.replacement;
 
+import entity.Team;
+
 public class ReplacementInputData {
+    private final Team currentTeam;
+
+    public ReplacementInputData(Team currentTeam) {
+        this.currentTeam = currentTeam;
+    }
+
+    public Team getCurrentTeam() {
+        return currentTeam;
+    }
 }
--- src/main/java/use_case/replacement/ReplacementOutputData.java
+++ src/main/java/use_case/replacement/ReplacementOutputData.java
@@ -1,4 +1,16 @@
 package use_case.replacement;
 
+import entity.Player;
+import java.util.List;
+
 public class ReplacementOutputData {
+    private final List<Player> suggestedPlayers;
+
+    public ReplacementOutputData(List<Player> suggestedPlayers) {
+        this.suggestedPlayers = suggestedPlayers;
+    }
+
+    public List<Player> getSuggestedPlayers() {
+        return suggestedPlayers;
+    }
 }
