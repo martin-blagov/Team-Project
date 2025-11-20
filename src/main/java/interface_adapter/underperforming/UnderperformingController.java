e8b67ffc44f24178ffa0d067ab6f43dcfd048a50
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2025-11-20 16:57:22 -0500

    implement basic controller and presenter

--- src/main/java/interface_adapter/underperforming/UnderperformingController.java
+++ src/main/java/interface_adapter/underperforming/UnderperformingController.java
@@ -1,4 +1,18 @@
 package interface_adapter.underperforming;
 
+import use_case.underperforming.UnderperformingInputBoundary;
+import use_case.underperforming.UnderperformingInputData;
+import use_case.underperforming.UnderperformingOutputData;
+
 public class UnderperformingController {
+
+    private final UnderperformingInputBoundary interactor;
+
+    public UnderperformingController(UnderperformingInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public UnderperformingOutputData handle(UnderperformingInputData inputData) {
+        return interactor.execute(inputData);
+    }
 }
--- src/main/java/interface_adapter/underperforming/UnderperformingPresenter.java
+++ src/main/java/interface_adapter/underperforming/UnderperformingPresenter.java
@@ -1,4 +1,13 @@
 package interface_adapter.underperforming;
 
-public class UnderperformingPresenter {
+import use_case.underperforming.UnderperformingOutputBoundary;
+import use_case.underperforming.UnderperformingOutputData;
+
+public class UnderperformingPresenter implements UnderperformingOutputBoundary {
+
+    @Override
+    public void presentUnderperformingPlayers(UnderperformingOutputData outputData) {
+        // Minimal placeholder: print to console
+        System.out.println("Underperforming players: " + outputData.getUnderperformingPlayers());
+    }
 }
