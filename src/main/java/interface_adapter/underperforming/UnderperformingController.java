a870cd80d12da7e0b6ed923ea4d8fb56bb00238c
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2025-11-25 01:25:15 -0500

    refactor controller to follow CA and retrieve team from teamviewmodel

--- src/main/java/interface_adapter/underperforming/UnderperformingController.java
+++ src/main/java/interface_adapter/underperforming/UnderperformingController.java
@@ -2,17 +2,25 @@ package interface_adapter.underperforming;
 
 import use_case.underperforming.UnderperformingInputBoundary;
 import use_case.underperforming.UnderperformingInputData;
-import use_case.underperforming.UnderperformingOutputData;
+import interface_adapter.team_view.TeamViewModel;
+import entity.Team;
+
 
 public class UnderperformingController {
 
     private final UnderperformingInputBoundary interactor;
+    private final TeamViewModel teamViewModel;
 
-    public UnderperformingController(UnderperformingInputBoundary interactor) {
+    public UnderperformingController(UnderperformingInputBoundary interactor,
+                                     TeamViewModel teamViewModel) {
         this.interactor = interactor;
+        this.teamViewModel = teamViewModel;
     }
 
-    public UnderperformingOutputData handle(UnderperformingInputData inputData) {
-        return interactor.execute(inputData);
+    public void handle() {
+        Team team = teamViewModel.getTeam(); // may be null
+        UnderperformingInputData inputData = new UnderperformingInputData(team);
+
+        interactor.execute(inputData);
     }
 }
